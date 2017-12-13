ba2480195306569c22d19ccb6156a97fdfd35197
irfanahm <irfan.ahmedg8@gmail.com>
2017-12-13 12:37:31 +0530

    Add project files.

---
+++ msWebAppSite/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace msWebAppSite.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ msWebAppSite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(msWebAppSite.Startup))]
+namespace msWebAppSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
